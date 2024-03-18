8c2c9104d6aa34ad01f00fd8b8d3d90e4e2973b9
fromdwlee <104008390+fromdwlee@users.noreply.github.com>
2024-03-18 23:23:04 +0900

    daily commit

---
+++ class_2/10845.py
@@ -0,0 +1,31 @@
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+d = deque()
+n = int(input())
+
+for i in range(n):
+    command = input().split()
+
+    if command[0] == "push":
+        d.append(command[1])
+    elif command[0] == "pop":
+        if len(d) == 0:
+            print("-1")
+        else: print(d.popleft())
+    elif command[0] == "size":
+        print(len(d))
+    elif command[0] == "empty":
+        if len(d) == 0:
+            print("1")
+        else: print("0")
+    elif command[0] == "front":
+        if len(d) == 0:
+            print("-1")
+        else: print(d[0])
+    elif command[0] == "back":
+        if len(d) == 0:
+            print("-1")
+        else: print(d[-1])
